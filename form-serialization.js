aa8f2a36f70d61d9760a8b6fd78ddb52b3a3b228
sunnydcitruspunch <wiwisun18@gmail.com>
2024-05-10 21:10:20 -0600

    Add form serialization

---
+++ form-serialization.js
@@ -0,0 +1,23 @@
+function serializeForm(form) {
+  const obj = {};
+  const formData = new FormData(form);
+  for([key, value] of formData.entries()) {
+    if(obj.hasOwnProperty(key)) {
+      if(Array.isArray(obj[key])) {
+        obj[key].push(value)
+      } else {
+        obj[key] = [obj[key]]
+      }
+    } else {
+      obj[key] = value
+    }
+  }
+
+  return obj;
+}
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const formData = serializeForm(this);
+  console.log('Serialized Data:', formData);
+});
